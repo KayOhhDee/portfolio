66566f6dc6b801f35603aa09235835f092e95382
kod3180 <kod3180@gmail.com>
2019-08-21 04:10:33 +0000

    particle.js added

---
+++ app/assets/scripts/vendor.js
@@ -0,0 +1,6 @@
+import particlesJs from "particles.js";
+
+/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+particlesJS.load('particles--bg', '../../../app/assets/particles.json', function() {
+    
+});
